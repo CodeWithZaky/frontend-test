9c2f66dae08a5d2e3ab94474a259887f5b4668ce
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2025-02-14 06:25:03 +0700

    refactor : ui and theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,10 +6,12 @@ function Navbar() {
   return (
     <div className="relative">
       <div className="flex justify-between items-center border-stone-700 border-b h-[50px]">
-        <Link href="/" className="px-5">
+        <Link href="/" className="ml-5">
           Home
         </Link>
-        <DropdownMenu />
+        <div>
+          <DropdownMenu />
+        </div>
       </div>
     </div>
   );
@@ -30,20 +32,23 @@ const DropdownMenu = () => {
   return (
     <div className="relative">
       {session?.status === "unauthenticated" && (
-        <Link href="/login" className="px-5 cursor-pointer">
+        <Link
+          href="/login"
+          className="bg-gray-600 mr-5 px-4 py-1 text-center cursor-pointer"
+        >
           Login
         </Link>
       )}
       {session?.status === "authenticated" && (
         <div>
           <span
-            className="px-5 cursor-pointer"
+            className="hover:bg-stone-600 mr-5 px-4 py-1 border border-gray-600 rounded-md text-center cursor-pointer"
             onClick={() => setIsOpenDropdown(!isOpenDropdown)}
           >
             {session.user?.username}
           </span>
           {isOpenDropdown && (
-            <div className="right-0 z-10 absolute bg-stone-900 mt-2 py-1 border border-stone-700 rounded-md w-48">
+            <div className="right-0 z-10 absolute bg-stone-900 mt-2 border border-stone-700 rounded-md w-48">
               <Link
                 href="/profile"
                 className="block hover:bg-stone-600 px-4 py-2 text-gray-200 text-sm"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,50 @@
+import ThemeSwitcher from "@/components/theme-switcher";
 import useSession from "@/hooks/useSession";
+import Link from "next/link";
 
 export default function Home() {
   const session = useSession();
 
   return (
-    <div className="flex justify-center items-center gap-5 min-h-72">
+    <div className="flex justify-center items-center gap-5 py-20 min-h-72">
       <div className="flex flex-col gap-5">
-        <p>Home Page</p>
-        <div>
-          {session?.status === "authenticated" && (
+        <h1 className="mx-auto text-3xl">HOME PAGE</h1>
+        <Sparator />
+        <div className="space-y-1">
+          <p className="font-bold">Change Theme</p>
+          <ThemeSwitcher />
+        </div>
+        <Sparator />
+        <div className="space-y-1">
+          <p className="font-bold">User Data</p>
+
+          {session?.status === "unauthenticated" ? (
             <>
-              <p>email: {session.user.username}</p>
-              <p>passwprd: {session.user.password}</p>
+              <p className="font-bold text-red-900">You are not logged in</p>
+              <div className="flex items-center gap-2">
+                <p>Login now</p>
+                <Link
+                  href="/login"
+                  className="bg-gray-600 px-4 py-1 text-center"
+                >
+                  Login
+                </Link>
+              </div>
+            </>
+          ) : (
+            <>
+              <div>
+                {session?.status === "authenticated" && (
+                  <>
+                    <p className="text-stone-400">
+                      username: {session.user.username}
+                    </p>
+                    <p className="text-stone-400">
+                      password: {session.user.password}
+                    </p>
+                  </>
+                )}
+              </div>
             </>
           )}
         </div>
@@ -19,3 +52,5 @@ export default function Home() {
     </div>
   );
 }
+
+const Sparator = () => <div className="bg-stone-700 w-full h-px" />;
